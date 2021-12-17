ff16faf8877ced2ecf3fb8c263e8189e2d323b5a
VladKornilov <36070619+VladKornilov@users.noreply.github.com>
2021-12-17 21:06:21 +0300

    Lab 3 - Redis

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,21 +2,40 @@ package server
 
 import (
 	"../api"
+	"../entities"
 	"../logger"
+	"context"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"net/http"
 	"os"
+	"time"
 )
+
+var redisClient *redis.Client
+var ctx = context.Background()
 
 func StartServer(router *gin.Engine) {
 	router.LoadHTMLGlob("html/templates/*.html")
 	addPageListeners(router)
 
+	url, _ := os.LookupEnv("SITE_URL")
 	port, _ := os.LookupEnv("SITE_PORT")
+	redisClient = SetupRedis(url, port)
 	err := router.Run(port)
 	if logger.LogErr(err) {
 		return
 	}
+}
+
+func SetupRedis(url string, port string) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     url + ":" + port,
+		Password: "",
+		DB:       0,
+	})
+	return client
 }
 
 func addPageListeners(router *gin.Engine) {
@@ -31,9 +50,22 @@ func startPage(c *gin.Context) {
 
 func handleWeatherRequest(c *gin.Context) {
 	city := c.Query("city")
-	weather, err := api.GetWeather(city)
+
+	var weather entities.Weather
+
+	weatherJson, err := redisClient.Get(ctx, city).Bytes()
 	if logger.LogErr(err) {
-		return
+		weather, err = api.GetWeather(city)
+		json, err := json.Marshal(weather)
+		redisClient.Set(ctx, city, json, time.Minute)
+		if logger.LogErr(err) {
+			return
+		}
+	} else {
+		err = json.Unmarshal(weatherJson, &weather)
+		if logger.LogErr(err) {
+			return
+		}
 	}
 
 	c.HTML(http.StatusOK, "update", gin.H{
